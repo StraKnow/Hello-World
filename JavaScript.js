ebac2d3ddeaeab85572e882bd59f33d6343fd1ae
StraKnow <straknow@gmail.com>
2016-04-13 05:37:39 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,4 @@
-function lighton() {
-    document.getElementById('myimage').src = "semaverde.gif";
-}
-function lightoff() {
-    document.getElementById('myimage').src = "semrojo.gif";
-}
 
 function mensaje(){
-	alert("Esta saliendo de StraKnow");
+	alert("Hola planeta");
 }
